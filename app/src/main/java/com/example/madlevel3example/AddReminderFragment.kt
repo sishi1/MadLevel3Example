af1d502182bebfba9ec001e42e7555101e53d15f
Sishi <sishi_hu12@hotmail.com>
2020-09-28 10:14:57 +0200

    Added step 4.4

--- app/src/main/java/com/example/madlevel3example/AddReminderFragment.kt
+++ app/src/main/java/com/example/madlevel3example/AddReminderFragment.kt
@@ -6,11 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_reminder.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
+
+const val REQ_REMINDER_KEY = "req_reminder"
+const val BUNDLE_REMINDER_KEY = "bundle_reminder"
+
 class AddReminderFragment : Fragment() {
 
     override fun onCreateView(
@@ -24,5 +32,18 @@ class AddReminderFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnAddReminder.setOnClickListener {
+            onAddReminder()
+        }
+    }
+
+    private fun onAddReminder() {
+        val reminderText = etReminderName.text.toString()
+
+        if (reminderText.isNotBlank()) {
+            setFragmentResult(REQ_REMINDER_KEY, bundleOf(Pair(BUNDLE_REMINDER_KEY, reminderText)))
+            findNavController().popBackStack()
+        } else
+            Toast.makeText(activity, R.string.not_valid_reminder, Toast.LENGTH_SHORT).show()
     }
 }
